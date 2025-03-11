3cdef9af3798d283fa9e36f487ac49934a20103f
josehb96 <josehurtadobaeza@gmail.com>
2025-03-11 05:40:18 +0100

    modificadores parte 2: alias

---
+++ Statements/15_alias.sql
@@ -0,0 +1,3 @@
+SELECT name, init_date AS "Fecha de inicio en programación" FROM users WHERE name = "Brais";
+
+SELECT name, init_date AS 'Fecha de inicio en programación' FROM users WHERE name = 'Brais';
