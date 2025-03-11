c94b4c3e5e172d166ff360e49dae2edaf14df4b0
josehb96 <josehurtadobaeza@gmail.com>
2025-03-11 06:04:56 +0100

    modificadores parte 2: concat

--- Statements/15_alias.sql
+++ Statements/15_alias.sql
@@ -1,3 +1,9 @@
 SELECT name, init_date AS "Fecha de inicio en programación" FROM users WHERE name = "Brais";
 
 SELECT name, init_date AS 'Fecha de inicio en programación' FROM users WHERE name = 'Brais';
+
+SELECT CONCAT(name, surname) FROM users;
+
+SELECT CONCAT('Nombre: ', name, ' Apellidos: ', surname) FROM users;
+
+SELECT CONCAT('Nombre: ', name, ' Apellidos: ', surname) AS "Nombre completo" FROM users;
