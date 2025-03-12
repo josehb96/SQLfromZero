45716acfde25570340f9616bb19d17c2659f6d05
josehb96 <josehurtadobaeza@gmail.com>
2025-03-12 21:27:02 +0100

    table administration: alter table

---
+++ Tables/03_alter_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE people8 (
+	id int NOT NULL AUTO_INCREMENT,
+    name varchar(100) NOT NULL,
+    age int,
+    email varchar(50),
+    created datetime DEFAULT CURRENT_TIMESTAMP(),
+    PRIMARY KEY(id),
+    CHECK(age >= 18)
+);
+
+ALTER TABLE people8 
+ADD surname varchar(150);
