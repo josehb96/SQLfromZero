6ce842d6fa08bdc100b27e18ac8ef6135defd587
josehb96 <josehurtadobaeza@gmail.com>
2025-03-12 05:29:12 +0100

    modificadores parte 2: ifnull

--- Statements/08_null.sql
+++ Statements/08_null.sql
@@ -3,3 +3,5 @@ SELECT * FROM users WHERE email IS NULL;
 SELECT * FROM users WHERE email IS NOT NULL; 
 
 SELECT * FROM users WHERE email IS NOT NULL AND age = 15; 
+
+SELECT name, surname, IFNULL(age, 0) AS age FROM users;
