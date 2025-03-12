c277390ba102873e62d7a0b386043a3ffc0f23ec
josehb96 <josehurtadobaeza@gmail.com>
2025-03-12 18:46:29 +0100

    table administration: unique

--- Tables/01_create_table.sql
+++ Tables/01_create_table.sql
@@ -14,3 +14,11 @@ CREATE TABLE people2 (
     created date
 );
 
+CREATE TABLE people3 (
+	id int NOT NULL,
+    name varchar(100) NOT NULL,
+    age int,
+    email varchar(50),
+    created datetime,
+    UNIQUE(id)
+);
