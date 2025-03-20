19a17ea011b494a9063d08d28e7f91e23d195125
josehb96 <josehurtadobaeza@gmail.com>
2025-03-20 04:51:44 +0100

    creating related tables: 1:N

--- Tables/04_relationships.sql
+++ Tables/04_relationships.sql
@@ -1,3 +1,4 @@
+-- 1:1
 CREATE TABLE dni(
 	dni_id int AUTO_INCREMENT PRIMARY KEY,
     dni_number int NOT NULL,
@@ -5,3 +6,18 @@ CREATE TABLE dni(
     UNIQUE(dni_id),
     FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
+
+-- 1:N
+CREATE TABLE companies(
+	company_id int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(100) NOT NULL
+);
+
+ALTER TABLE users
+ADD company_id varchar(100);
+
+ALTER TABLE users
+ADD CONSTRAINT fk_companies
+FOREIGN KEY(company_id) REFERENCES companies(company_id);
+
+-- N:M
