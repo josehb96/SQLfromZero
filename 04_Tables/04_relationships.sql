9f1cb67e3b519dac7eb6d23998c370cbf392e9af
josehb96 <josehurtadobaeza@gmail.com>
2025-03-20 18:33:21 +0100

    storage of related data: 1:1 data

--- 04_Tables/04_relationships.sql
+++ 04_Tables/04_relationships.sql
@@ -34,3 +34,11 @@ CREATE TABLE users_languages(
     FOREIGN KEY(language_id) REFERENCES languages(language_id),
     UNIQUE(user_id, language_id)
 );
+
+-- INSERT
+
+-- DATA 1:1
+INSERT INTO dni(dni_number, user_id) VALUES (11111111, 1);
+INSERT INTO dni(dni_number, user_id) VALUES (22222222, 2);
+INSERT INTO dni(dni_number, user_id) VALUES (33333333, 3);
+INSERT INTO dni(dni_number) VALUES (44444444);
