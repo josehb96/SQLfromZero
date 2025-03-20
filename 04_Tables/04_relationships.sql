24450f125c92811eff7444c5bcb2a642892086cc
josehb96 <josehurtadobaeza@gmail.com>
2025-03-20 18:34:08 +0100

    storage of related data: 1:N data

--- 04_Tables/04_relationships.sql
+++ 04_Tables/04_relationships.sql
@@ -37,8 +37,21 @@ CREATE TABLE users_languages(
 
 -- INSERT
 
--- DATA 1:1
+-- 1:1 DATA
+
 INSERT INTO dni(dni_number, user_id) VALUES (11111111, 1);
 INSERT INTO dni(dni_number, user_id) VALUES (22222222, 2);
 INSERT INTO dni(dni_number, user_id) VALUES (33333333, 3);
 INSERT INTO dni(dni_number) VALUES (44444444);
+
+-- 1:N DATA
+
+INSERT INTO companies (name) VALUES ('MoureDev');
+INSERT INTO companies (name) VALUES ('Apple');
+INSERT INTO companies (name) VALUES ('Google');
+INSERT INTO companies (company_id) VALUES (5); -- Este falla porque name es NOT NULL
+
+UPDATE users SET company_id = 1 WHERE user_id = 1;
+UPDATE users SET company_id = 2 WHERE user_id = 3;
+UPDATE users SET company_id = 3 WHERE user_id = 4;
+UPDATE users SET company_id = 1 WHERE user_id = 7;
