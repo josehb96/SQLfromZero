b7fd8eccb68f067e922890c13830fbda1e4d411e
josehb96 <josehurtadobaeza@gmail.com>
2025-03-20 04:58:28 +0100

    creating related tables: N:M

--- Tables/04_relationships.sql
+++ Tables/04_relationships.sql
@@ -21,3 +21,16 @@ ADD CONSTRAINT fk_companies
 FOREIGN KEY(company_id) REFERENCES companies(company_id);
 
 -- N:M
+CREATE TABLE languages(
+	language_id int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(100) NOT NULL
+);
+
+CREATE TABLE users_languages(
+	users_language_id int AUTO_INCREMENT PRIMARY KEY,
+    user_id int,
+    language_id int, 
+    FOREIGN KEY(user_id) REFERENCES users(user_id),
+    FOREIGN KEY(language_id) REFERENCES languages(language_id),
+    UNIQUE(user_id, language_id)
+);
