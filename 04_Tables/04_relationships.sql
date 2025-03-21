19d6e868888a3f2d0f725739ce77dac0a8f3d0e2
josehb96 <josehurtadobaeza@gmail.com>
2025-03-21 04:11:23 +0100

    storage of related data: N:M data

--- 04_Tables/04_relationships.sql
+++ 04_Tables/04_relationships.sql
@@ -55,3 +55,22 @@ UPDATE users SET company_id = 1 WHERE user_id = 1;
 UPDATE users SET company_id = 2 WHERE user_id = 3;
 UPDATE users SET company_id = 3 WHERE user_id = 4;
 UPDATE users SET company_id = 1 WHERE user_id = 7;
+
+-- N:M DATA
+
+INSERT INTO languages (name) VALUES ('Swift');
+INSERT INTO languages (name) VALUES ('Kotlin');
+INSERT INTO languages (name) VALUES ('JavaScript');
+INSERT INTO languages (name) VALUES ('Java');
+INSERT INTO languages (name) VALUES ('Python');
+INSERT INTO languages (name) VALUES ('C#');
+INSERT INTO languages (name) VALUES ('COBOL');
+
+
+INSERT INTO users_languages (user_id, language_id) VALUES (1, 1);
+INSERT INTO users_languages (user_id, language_id) VALUES (1, 2);
+INSERT INTO users_languages (user_id, language_id) VALUES (1, 5);
+INSERT INTO users_languages (user_id, language_id) VALUES (2, 3);
+INSERT INTO users_languages (user_id, language_id) VALUES (2, 5);
+INSERT INTO users_languages (user_id, language_id) VALUES (1, 1); -- Este falla porque la combinación de user_id y language_id ya existe
+INSERT INTO users_languages (user_id) VALUES (1); -- Este falla porque language_id es NOT NULL
