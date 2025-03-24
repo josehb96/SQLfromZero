adc7c4551ce4bcbbf62b5b9b2abe231d4295b78a
josehb96 <josehurtadobaeza@gmail.com>
2025-03-24 03:10:58 +0100

    advanced concepts: trigger

---
+++ 06_Advanced/02_trigger.sql
@@ -0,0 +1,24 @@
+CREATE TABLE `hello_mysql`.`email_history` (
+  `email_history_id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `email` VARCHAR(100) NULL,
+  PRIMARY KEY (`email_history_id`),
+  UNIQUE INDEX `idemail_history_UNIQUE` (`email_history_id` ASC) VISIBLE);
+
+delimiter | -- El límitador lo que hace simplemente es guardar esta referencia por separado
+
+CREATE TRIGGER tg_email
+AFTER UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF OLD.email <> NEW.email THEN
+		INSERT INTO email_history (user_id, email)
+        VALUES (OLD.user_id, OLD.email);
+	END IF;
+END;
+
+|
+
+delimiter ;
+
+UPDATE users SET email = "mouredev@gmail.com" WHERE user_id = 1;
