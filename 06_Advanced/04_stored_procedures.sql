b9b1b2bad3510b70341af9d24f9976deaba51233
josehb96 <josehurtadobaeza@gmail.com>
2025-03-24 16:53:30 +0100

    advanced concepts: stored procedures

---
+++ 06_Advanced/04_stored_procedures.sql
@@ -0,0 +1,17 @@
+DELIMITER //
+CREATE PROCEDURE p_all_users()
+BEGIN
+	SELECT * FROM users;
+END//
+
+CALL p_all_users()
+
+DELIMITER //
+CREATE PROCEDURE p_age_users(IN age_param int)
+BEGIN
+	SELECT * FROM users WHERE age = age_param;
+END//
+
+CALL p_age_users(30);
+
+DROP PROCEDURE p_age_users; -- to delete the procedure
